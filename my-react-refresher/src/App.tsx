d89c5a01b78b8c3549a738248c72df79eaf1c82a
asharma <asharma@konradgroup.com>
2025-07-07 16:36:11 +0530

    Added second app with CRD functionality.

--- my-react-refresher/src/App.tsx
+++ my-react-refresher/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Counter from '../src/Components/Counter-app/Counter';
+import Todo from './Components/Todo-app/Todo';
 
 function App() {
 
@@ -7,7 +8,8 @@ function App() {
   return (
     <>
       <h1>Hey! Welcome to my react refresher</h1>
-      <Counter/>
+      {/* <Counter/> */}
+      <Todo/>
     </>
   )
 }
--- my-react-refresher/src/Components/Todo-app/Todo.tsx
+++ my-react-refresher/src/Components/Todo-app/Todo.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { useState } from "react";
+
+export interface Todo {
+    id: string;
+    title: string;
+    isCompleted: boolean;
+}
 
 const Todo = () => {
-  return (
-    <div>Todo</div>
-  )
+    const [todos, setTodos] = useState<Todo[]>([]);
+    const [newTodo, setNewTodo] = useState('');
+    const handleAddNewTodo = () => {
+        if (!newTodo.trim()) alert('Please add a todo!');
+        const newItem: Todo = {
+            id: Math.random() + '',
+            title: newTodo.trim(),
+            isCompleted: false
+        }
+        setTodos(prev => [...prev, newItem]);
+        setNewTodo('')
+    }
+    const handleComplete = (id:string) => {
+        setTodos(prev => prev.map((todo)=>todo.id===id ? {...todo,isCompleted:!todo.isCompleted} : todo))
+    }
+    const handleDelete = (id:string) => {
+        setTodos(prev=>prev.filter((todo)=>todo.id!== id))
+    }
+
+    return (
+        <>
+            <div>Todo</div>
+            <input
+                type="text"
+                value={newTodo}
+                onChange={(e) => setNewTodo(e.target.value)}
+                placeholder="Add todo"
+            />
+            <button onClick={handleAddNewTodo}>Add new Todo</button>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        <input
+                            type="checkbox"
+                            checked={todo.isCompleted}
+                            onChange={() => handleComplete(todo.id)}
+                        />
+                        {todo.title}
+                        <button onClick={()=>handleDelete(todo.id)}>Delete</button>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default Todo
